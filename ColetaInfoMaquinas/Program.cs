2227dc0b073d0043909006a7d1d664e4634d8f99
Danilo Fernando <danilo.bossanova@hotmail.com>
2024-06-04 18:19:42 -0300

    Adicionar arquivos de projeto.

---
+++ ColetaInfoMaquinas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Management;
+
+namespace ColetaInfoMaquinas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Coletando as informações
+                string os = GetOSInfo();
+
+
+
+                /****************************************************************
+                 Funções que Buscam as informações da maquina 
+                ****************************************************************/
+
+                string GetOSInfo()
+                {
+                    return Environment.OSVersion.VersionString;
+                }
+
+            }
+            catch { 
+                
+            }
+        }
+    }
+}
